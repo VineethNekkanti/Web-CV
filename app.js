650d2d6a825f80640844d7861585cebe4416a7ca
VineethNekkanti <vineethnekkanti314@gmail.com>
2023-03-05 16:16:49 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function(req,res){
+    res.render("cv");
+});
+
+app.get("/story",function(req,res){
+  res.render("story")
+});
+
+app.get("/cycling",function(req,res){
+  res.render("cycling")
+});
+
+app.get("/webdev",function(req,res){
+  res.render("webdev")
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
